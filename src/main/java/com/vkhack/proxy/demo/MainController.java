afc40b9dd4fc3291a44e30d4618929a9ab288b40
Egor <yakovlev.es@phystech.edu>
2020-09-15 01:45:13 +0300

    Initial commit

---
+++ src/main/java/com/vkhack/proxy/demo/MainController.java
@@ -0,0 +1,32 @@
+package com.vkhack.proxy.demo;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class MainController {
+    private final RestTemplate restTemplate;
+
+    public MainController(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @GetMapping("/proxy")
+    @ResponseBody
+    public MultipartFile sendRequestForGetImage() {
+        String url = "https://service.pavel.im/image";
+        ResponseEntity<MultipartFile> response = this.restTemplate.getForEntity(url, MultipartFile.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+}
