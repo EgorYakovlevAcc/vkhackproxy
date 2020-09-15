dbfbafc9cbce471794b344aab648db505351405e
Egor Yakovlev <36803548+EgorYakovlevAcc@users.noreply.github.com>
2020-09-15 11:02:57 +0300

    fix https issue

--- src/main/java/com/vkhack/proxy/demo/MainController.java
+++ src/main/java/com/vkhack/proxy/demo/MainController.java
@@ -26,7 +26,15 @@ public class MainController {
     @GetMapping("/proxy")
     @ResponseBody
     public Object sendRequestForGetImage() {
+        CloseableHttpClient httpClient = HttpClients.custom()
+                    .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                    .build();
+HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+requestFactory.setHttpClient(httpClient);
+RestTemplate restTemplate = new RestTemplate(requestFactory);
         String url = "https://service.pavel.im/image";
+Object response = restTemplate.getForObject(url, Object.class);
+        
         ResponseEntity<Object> response = this.restTemplate.getForEntity(url, Object.class);
         if (response.getStatusCode() == HttpStatus.OK) {
             return response.getBody();
