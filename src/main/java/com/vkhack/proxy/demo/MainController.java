fe7d9484757d967e94b71bb0c6f08eade0589e20
Eugene12 <voznesenskiy.evgeniy@mail.ru>
2020-09-15 11:59:56 +0300

    add CrossOrigin

--- src/main/java/com/vkhack/proxy/demo/MainController.java
+++ src/main/java/com/vkhack/proxy/demo/MainController.java
@@ -4,11 +4,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
+@CrossOrigin
 public class MainController {
     @GetMapping
     public String getIndex() {
