57f42418bf88e9d90f7582888cae3a841751c0ea
Eugene12 <voznesenskiy.evgeniy@mail.ru>
2020-09-15 11:51:08 +0300

    сhange produces

--- src/main/java/com/vkhack/proxy/demo/MainController.java
+++ src/main/java/com/vkhack/proxy/demo/MainController.java
@@ -1,15 +1,12 @@
 package com.vkhack.proxy.demo;
 
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-
-import java.awt.image.BufferedImage;
 
 @Controller
 public class MainController {
@@ -18,15 +15,12 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/proxy")
+    @GetMapping(value = "/proxy", produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseBody
     public Object sendRequestForGetImage() {
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://service.pavel.im/image";
         ResponseEntity<byte[]> response = restTemplate.exchange(url, HttpMethod.GET, null, byte[].class);
-        System.out.println(response.getBody());
-        System.out.println(response.getStatusCode());
-        System.out.println(response.getHeaders());
         return response.getBody();
     }
 }
